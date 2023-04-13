f91f4402fb27546c20f5b01e95af805380bba439
Elias Breitenbach <elias.b@me.com>
2023-04-13 20:16:43 +0200

    v1

--- Day26pj4WorkingWithDates/ContentView.swift
+++ Day26pj4WorkingWithDates/ContentView.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-
-
+        VStack {
+            Text(Date.now, format: .dateTime.hour().minute())
+                .padding()
+            Text(Date.now, format: .dateTime.day().month().year())
+                .padding()
+            Text(Date.now.formatted(date: .long, time: .shortened))
+                .padding()
+            Text(Date.now.formatted(date: .long, time: .omitted))
+        }
     }
+    //        func trivalExample() {
+    //            let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+    //            let hour = components.hour ?? 0
+    //            let minute = components.minute ?? 0
+    
+    //    }
 }
 
 struct ContentView_Previews: PreviewProvider {
