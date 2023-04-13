1dac7332ea62b94a9f2dca02e17a1614fa5ba972
Elias Breitenbach <elias.b@me.com>
2023-04-13 19:55:41 +0200

    Create a Project

---
+++ Day26pj4WorkingWithDates/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day26pj4WorkingWithDates
+//
+//  Created by Elias Breitenbach on 13.04.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day26pj4WorkingWithDates/Day26pj4WorkingWithDatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day26pj4WorkingWithDatesApp.swift
+//  Day26pj4WorkingWithDates
+//
+//  Created by Elias Breitenbach on 13.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Day26pj4WorkingWithDatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
